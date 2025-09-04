f070493ec31c7921e89cd8a145f863bb71dd1832
James McMichael <122940945+texjames1515@users.noreply.github.com>
2025-09-04 15:16:25 -0400

    Created button that when clicked textview text is modified

--- app/src/main/java/edu/temple/helloworldapp/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworldapp/MainActivity.kt
@@ -1,17 +1,26 @@
 package edu.temple.helloworldapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState) //do not touch this line will crash
         setContentView(R.layout.activity_main) //do not touch, will not show view
 
 
+
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            findViewById<TextView>(R.id.textView).text = "Why did you click the button?"
+        }
 
     }
 }
